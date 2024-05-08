a9e67db022ec83c9da403d4ab28da37984f9035e
X-pose <falconbusiness123@gmail.com>
2024-05-08 16:49:11 +0530

    User service basic features added

--- UserService/app.js
+++ UserService/app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const cors = require("cors")
 const connectDB = require('./config/database')
+const learnerRoutes = require('./src/routes/learnerRoutes')
 
 connectDB.getInstance()
 
@@ -10,6 +11,7 @@ app.use(cors())
 app.use(express.json())
 
 //Add routes here
+app.use('/learner', learnerRoutes)
 
 //Exporting app to be used by the server.js
 module.exports = app
