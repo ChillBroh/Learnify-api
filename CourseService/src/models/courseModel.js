be4715b64cf7a9c683a78b3dcafe04e829bef39b
ChillBroh <isharamadusanka410@gmail.com>
2024-05-09 16:19:48 +0530

    course model updated

--- CourseService/src/models/courseModel.js
+++ CourseService/src/models/courseModel.js
@@ -10,8 +10,20 @@ const quizSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  options: {
-    type: [String],
+  option1: {
+    type: String,
+    required: true,
+  },
+  option2: {
+    type: String,
+    required: true,
+  },
+  option3: {
+    type: String,
+    required: true,
+  },
+  option4: {
+    type: String,
     required: true,
   },
   correctAnswer: {
