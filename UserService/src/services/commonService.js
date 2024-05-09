56ebe92724c2caaa58c6673d508425c265fa8b49
X-pose <falconbusiness123@gmail.com>
2024-05-09 15:06:14 +0530

    Get all users by type feature added

--- UserService/src/services/commonService.js
+++ UserService/src/services/commonService.js
@@ -1,4 +1,4 @@
-const { saveUserPreferences, deleteUser } = require('../controllers/learnerController')
+const { saveUserPreferences, deleteUser, getUsersByType } = require('../controllers/commonController')
 
 
 exports.updateUser = async(req,res) => {
@@ -12,7 +12,7 @@ exports.updateUser = async(req,res) => {
     res.status(response.status).json(response.body)
 } 
 
-exports.deleteLearner = async(req,res) => {
+exports.deleteUser = async(req,res) => {
     const name = req.body.userName
 
     const response = await deleteUser(name)
@@ -20,3 +20,11 @@ exports.deleteLearner = async(req,res) => {
     res.status(response.status).json(response.body)
 }
 
+exports.getAllUserByType = async(req,res) => {
+
+    const userType = req.params.id
+
+    const response = await getUsersByType(userType)
+
+    res.status(response.status).json(response.body)
+}
