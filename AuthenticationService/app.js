9cb1b627ec95b4ffd5f2a8a26d71e6f9ead23230
ChillBroh <isharamadusanka410@gmail.com>
2024-05-11 01:19:22 +0530

    docker files added to all services

--- AuthenticationService/app.js
+++ AuthenticationService/app.js
@@ -1,23 +1,19 @@
-const express = require("express")
-const app = express()
-const appRouter = express.Router()
-const cors = require("cors")
-const connectDB = require('./config/database')
+const express = require("express");
+const app = express();
+const appRouter = express.Router();
+const cors = require("cors");
+const connectDB = require("./config/database");
 
-//Requires - Route classes 
-const authRoutes = require('./src/routes/authRoutes')
+//Requires - Route classes
+const authRoutes = require("./src/routes/authRoutes");
 
-connectDB.getInstance()
+connectDB.getInstance();
 
+app.use(cors());
 
-app.use(cors())
+app.use(express.json());
 
-app.use(express.json())
-app.use('/', appRouter)
-
-app.use('/server', authRoutes)
-
-
+app.use("/", authRoutes);
 
 //Exporting app to be used by the server.js
-module.exports = app
+module.exports = app;
--- AuthenticationService/server.js
+++ AuthenticationService/server.js
@@ -3,21 +3,18 @@
  */
 
 //Imports and Requires
-const http = require('http')
-const app = require('./app')
+const http = require("http");
+const app = require("./app");
 
-require('dotenv').config()
+require("dotenv").config();
 
-console.log( 'Server Js executing... Initiating HTTP server')
-const port = process.env.PORT
-
+console.log("Server Js executing... Initiating HTTP server");
+const port = process.env.PORT;
 
 //Creates HTTP server
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 //Server then  listen to the port (4000)
-server.listen(port, () => {
-   
-    console.log(`Authentication Server is running on port ${port}`)
-
-})
+server.listen(8001, () => {
+  console.log(`Authentication Server is running on port ${port}`);
+});
