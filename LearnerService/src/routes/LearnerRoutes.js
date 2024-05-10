428a55af3a098f20a85fc454aeeb7296a91c5a81
ChillBroh <isharamadusanka410@gmail.com>
2024-05-11 02:43:39 +0530

    learner servie bug fixed

--- LearnerService/src/routes/LearnerRoutes.js
+++ LearnerService/src/routes/LearnerRoutes.js
@@ -3,17 +3,19 @@
  */
 
 //Requires
-const express = require('express')
-const router = express.Router()
-const {enrolledCourses,completedCoursesList} = require('../services/LearnerService')
+const express = require("express");
+const router = express.Router();
+const {
+  enrolledCourses,
+  completedCoursesList,
+} = require("../services/LearnerService");
 
+router.get("/completed/:id", async (req, res) => {
+  await completedCoursesList(req, res);
+});
 
-router.get('/completed/:id', async(req,res) => {
-    await completedCoursesList(req,res)
-})
-
-router.get('/enrolled/:id', async(req,res) => {
-    await enrolledCourses(req,res)
-})
+router.get("/enrolled/:id", async (req, res) => {
+  await enrolledCourses(req, res);
+});
 //Exporting router to be used by the app.js
-module.exports = router
+module.exports = router;
