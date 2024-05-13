e900cb6372565f19d302bd9c67d5d98186c1ff5e
ChillBroh <isharamadusanka410@gmail.com>
2024-05-13 14:17:39 +0530

    notification service email connected

--- NotificationService/src/routers/Routers.js
+++ NotificationService/src/routers/Routers.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const EmailController = require("../controllers/EmailController");
 const SMSController = require("../controllers/SMSController");
 
-router.post("/sendMail", EmailController);
-router.post("/sendSMS", SMSController);
+router.post("/send-email", EmailController);
+router.post("/send-sms", SMSController);
 
 module.exports = router;
--- PaymentService/app.js
+++ PaymentService/app.js
@@ -1,15 +1,18 @@
-const express = require("express")
-const cors = require("cors")
-const connectDB = require('./config/database')
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/database");
+const stripeRouter = require("./src/routes/stripeRouter");
 
-connectDB.getInstance()
+connectDB.getInstance();
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+
+app.use("/", stripeRouter);
 
 //Add routes here
 
 //Exporting app to be used by the server.js
-module.exports = app
+module.exports = app;
